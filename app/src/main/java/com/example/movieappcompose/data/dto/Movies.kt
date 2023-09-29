83019857e65fc52d7854ea6418865f403bea2bd2
ozancanguz <ozancanguz24@gmail.com>
2023-09-29 12:41:04 +0300

    Dto(data transfer objects) are created as model.

---
+++ app/src/main/java/com/example/movieappcompose/data/dto/Movies.kt
@@ -0,0 +1,7 @@
+package com.example.movieappcompose.data.dto
+
+data class Movies(
+    val Response: String,
+    val Search: List<Search>,
+    val totalResults: String
+)
---
+++ app/src/main/java/com/example/movieappcompose/data/dto/Search.kt
@@ -0,0 +1,9 @@
+package com.example.movieappcompose.data.dto
+
+data class Search(
+    val Poster: String,
+    val Title: String,
+    val Type: String,
+    val Year: String,
+    val imdbID: String
+)
