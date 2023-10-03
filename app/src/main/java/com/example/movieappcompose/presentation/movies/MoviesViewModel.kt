6659d652743f3c51c39796f6e0c9b9098a5c952d
ozancanguz <ozancanguz24@gmail.com>
2023-10-03 11:33:43 +0300

    viewmodel created

---
+++ app/src/main/java/com/example/movieappcompose/presentation/movies/MoviesViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.movieappcompose.presentation.movies
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.movieappcompose.domain.useCase.getMovie.GetMovieUseCase
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class MoviesViewModel@Inject constructor(private val getMovieUseCase: GetMovieUseCase): ViewModel() {
+
+
+
+    private val _state= mutableStateOf<MoviesState>(MoviesState())
+    val state: State<MoviesState> =_state
+
+
+    private var job: Job? =null
+
+    fun getMovies(search:String){
+
+        job?.cancel()
+
+         job=  getMovieUseCase.executeGetMovies(search).onEach {
+
+             _state.value=MoviesState(false,it)
+
+
+
+      }.launchIn(viewModelScope)
+
+    }
+
+
+
+}
