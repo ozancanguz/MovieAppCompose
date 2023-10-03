d4679c7e147d2bc463b2e50bcccfe8daba5a2fb7
ozancanguz <ozancanguz24@gmail.com>
2023-10-03 16:28:30 +0300

    movie detail screen designed

--- app/src/main/java/com/example/movieappcompose/presentation/nav/MovieDetailsScreen.kt
+++ app/src/main/java/com/example/movieappcompose/presentation/nav/MovieDetailsScreen.kt
@@ -1,8 +1,93 @@
 package com.example.movieappcompose.presentation.nav
 
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterEnd
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberImagePainter
+import com.example.movieappcompose.presentation.movieDetails.MovieDetailsVieModel
+
 @Composable
-fun MovieDetailsScreen() {
+fun MovieDetailsScreen(
+    movieDetailViewModel: MovieDetailsVieModel = hiltViewModel()
+) {
+    val state = movieDetailViewModel.state.value
+
+    Box(modifier = Modifier.fillMaxSize()
+        .background(Color.Black)
+        ,contentAlignment = Center) {
+       state.movieDetail?.let {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(painter = rememberImagePainter(data = it.Poster),
+                    contentDescription = it.Title,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .size(300.dp, 300.dp)
+                        .clip(RectangleShape)
+                        .align(CenterHorizontally)
+                )
+
+                Text(text = it.Title,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(14.dp),
+                    color = Color.White
+                )
+
+                Text(text = it.Year,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(14.dp),
+                    color = Color.White
+                )
+
+                Text(text = it.Actors,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(14.dp),
+                    color = Color.White
+                )
+
+                Text(text = it.Country,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(14.dp),
+                    color = Color.White
+                )
+
+                Text(text = "Director: ${it.Director}",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(14.dp),
+                    color = Color.White
+                )
+
+                Text(text = "IMDB Rating: ${it.imdbRating}",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(14.dp),
+                    color = Color.White
+                )
+
+            }
+        }
+
+
+
+    }
+
 
 }
